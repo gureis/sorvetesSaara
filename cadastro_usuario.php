1c2952819d3494e529205916555359df0e1b0026
gureis <33041939+gureis@users.noreply.github.com>
2018-01-26 21:00:14 -0200

    testes de email/cpf/rg repetidos

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -19,7 +19,53 @@
 	$objDb = new db(); //recebe o db
 	$link = $objDb->conecta_mysql(); // função de conexão bd
 
-	$sql = "INSERT INTO usuarios(nome, sobrenome, sexo, cpf, rg, email, senha, telefone, nascimento, endereco, cidade, cep, estado) values ('$nome', '$sobrenome', '$sexo', '$cpf', '$rg', '$email', '$senha', '$telefone', '$nascimento', '$endereco', '$cidade', '$cep', '$estado')";
+	$email_existe = false;
+	$cpf_existe = false;
+	$rg_existe = false;
+
+	$sql = "SELECT email FROM usuarios WHERE email = '$email'";
+	$res = mysqli_query($link, $sql);
+	if($res) {
+		$user_data = mysqli_fetch_array($res);
+		if(isset($user_data['email'])) {
+			$email_existe = true;
+		}
+	}
+
+	$sql = "SELECT cpf FROM usuarios WHERE cpf = '$cpf'";
+	$res = mysqli_query($link, $sql);
+	if($res) {
+		$user_data = mysqli_fetch_array($res);
+		if(isset($user_data['cpf'])) {
+			$cpf_existe = true;
+		}
+	}
+
+	$sql = "SELECT rg FROM usuarios WHERE rg = '$rg'";
+	$res = mysqli_query($link, $sql);
+	if($res) {
+		$user_data = mysqli_fetch_array($res);
+		if(isset($user_data['rg'])) {
+			$rg_existe = true;
+		}
+	}
+
+	if($email_existe || $cpf_existe || $rg_existe) {
+		$return_get = '';
+
+		if($email_existe)
+			$return_get .= "err_email=1&";
+
+		if($cpf_existe)
+			$return_get .= "err_cpf=1&";
+
+		if($rg_existe)
+			$return_get .= "err_rg=1";
+
+		header("Location: index.html?".$return_get);
+	}
+
+	$sql = "INSERT INTO usuarios(nome, sobrenome, sexo, cpf, rg, email, senha, telefone, nascimento, endereco, cidade, cep, estado) VALUES ('$nome', '$sobrenome', '$sexo', '$cpf', '$rg', '$email', '$senha', '$telefone', '$nascimento', '$endereco', '$cidade', '$cep', '$estado')";
 
 	//Exec query
 	if (mysqli_query($link, $sql)){
