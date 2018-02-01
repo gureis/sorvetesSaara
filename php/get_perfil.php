e3a2d05af41dbaa0ee5baa5f0c1fc5485aca59e0
Victornnunes <35108448+Victornnunes@users.noreply.github.com>
2018-02-01 17:47:49 -0200

    update get user

--- php/get_perfil.php
+++ php/get_perfil.php
@@ -1,10 +1,4 @@
 <?php
-	session_start();
-
-	if(!isset($_SESSION['email'])) {
-		header("Location: index.html?erro=3");
-	}
-
 	require_once('db.class.php');
 
 	$email = $_SESSION['email'];
@@ -17,19 +11,22 @@
 	$res = mysqli_query($link, $sql);
 
 	if($res) {
-		$user_data = mysqli_fetch_array($res);
-		echo "<p>".$user_data['nome']."</p>";
-		echo "<p>".$user_data['sobrenome']."</p>";
-		echo "<p>".$user_data['sexo']."</p>";
-		echo "<p>".$user_data['cpf']."</p>";
-		echo "<p>".$user_data['rg']."</p>";
-		echo "<p>".$user_data['email']."</p>";
-		echo "<p>".$user_data['telefone']."</p>";
-		echo "<p>".date("d/m/y", strtotime($user_data['nascimento']))."</p>";
-		echo "<p>".$user_data['endereco']."</p>";
-		echo "<p>".$user_data['cidade']."</p>";
-		echo "<p>".$user_data['cep']."</p>";
-		echo "<p>".$user_data['estado']."</p>";
+		$user = mysqli_fetch_array($res);
+
+		$user_data = [
+			"nome" => $user['nome'],
+			"sobrenome" => $user['sobrenome'],
+			"telefone" => $user['telefone'],
+			"endereco" => $user['endereco'],
+			"cidade" => $user['cidade'],
+			"cep" => $user['cep'],
+			"estado" => $user['estado']
+			"sexo" => $user['sexo'],
+			"email" => $user['email'],
+			"nascimento" => $user['nascimento'],
+		];
+
+		echo json_encode($user_data);
 	} else {
 		echo "Erro na consulta das infos.";
 	}
