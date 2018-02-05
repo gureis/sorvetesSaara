e6bf92b04a938219518e61808ff5822e80eb9094
Gustavo Reis <gureis4@gmail.com>
2018-02-05 12:50:41 -0200

    Revert "Commit João"
    This reverts commit 05b9e5221b86428cbefecbb356b4276a4080d2bd, reversing
    changes made to 4e4aac6c52f15866d4d88ea90466d2445a3f3c79.

--- php/validar_acesso.php
+++ php/validar_acesso.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+
 	require_once("db.class.php");
 
 	$postdata = file_get_contents("php://input");
@@ -17,6 +19,7 @@
 	if ($res){
 		$dados_usuario = mysqli_fetch_array($res);
 		if(isset($dados_usuario['email'])) {
+			$_SESSION['email'] = $dados_usuario['email'];
 			$user = [
 				"status" => "Ok",
 				"login" => $dados_usuario['email'],
