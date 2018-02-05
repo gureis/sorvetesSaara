bb7ffd0a71d27f429c6615630276c7674b9af81c
gureis <33041939+gureis@users.noreply.github.com>
2018-02-05 16:38:26 -0200

    Add files via upload

--- php/get_perfil.php
+++ php/get_perfil.php
@@ -8,7 +8,7 @@
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
 
-	$email = $request->'login';
+	$email = $request->login;
 
 	$sql = "SELECT * FROM usuarios WHERE email = '$email'";
 
