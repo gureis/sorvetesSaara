8ff07f999dca437cec7637bfd6c216b6fdda7048
Felipe <felipe.custodio.f@gmail.com>
2018-02-05 12:57:39 -0200

    -a

--- php/get_perfil.php
+++ php/get_perfil.php
@@ -1,7 +1,6 @@
 <?php
 	require_once('db.class.php');
 
-<<<<<<< HEAD
 	$postdata = file_get_contents("php://input");
 	$request = json_decode($postdata);
 	
@@ -10,15 +9,6 @@
 
 	$objDb = new db(); //recebe o db
 	$link = $objDb->conecta_mysql(); // função de conexão bd
-=======
-	$objDb = new db();
-	$link = $objDb->conecta_mysql();
-
-	$postdata = file_get_contents("php://input");
-	$request = json_decode($postdata, true);
-
-	$email = $request["login"];
->>>>>>> 2b4557cc769b3078965d5ed09248a28b919c8ad6
 
 	$sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
 
