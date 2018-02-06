77bd605907eae6e79751d1a1d8759cde5f212a46
gureis <33041939+gureis@users.noreply.github.com>
2018-02-05 22:02:24 -0200

    Atualização var resposta

---
+++ update.php
@@ -0,0 +1,47 @@
+<?php
+	require_once('db.class.php');
+
+	$postdata = file_get_contents("php://input");
+	$request = json_decode($postdata);
+
+	$nome = $request->nome;
+	$sobrenome = $request->sobrenome;
+	$telefone = $request->telefone;
+	$endereco = $request->endereco;
+	$cidade = $request->cidade;
+	$cep = $request->cep;
+	$estado = $request->estado;
+	$email = $request->email;
+	$senha = $request->senha;
+	$sexo = $request->sexo;
+	$resposta = [
+		'senha' => "",
+		'status' => ""
+	];
+
+	if($senha=="")
+	{
+		$sql = "UPDATE usuarios SET nome='$nome', sobrenome='$sobrenome', telefone='$telefone', endereco='$endereco', cidade='$cidade', cep='$cep', estado='$estado', sexo='$sexo' WHERE email='$email'";
+	}else{
+		$senha = md5($senha);
+		$sql = "UPDATE usuarios SET nome='$nome', sobrenome='$sobrenome', telefone='$telefone', endereco='$endereco', cidade='$cidade', cep='$cep', estado='$estado', senha='$senha', sexo='$sexo' WHERE email='$email'";		
+	}
+
+	$objDb = new db();
+	$link = $objDb->conecta_mysql();
+
+
+	if(mysqli_query($link, $sql)){
+		if($senha != "")
+			$resposta['senha'] = $senha;
+		$resposta['status'] = "Alterações realizadas com sucesso";
+    	
+	} else {
+		$resposta['status'] = "Erro ao atualizar informações";
+	}
+
+	echo json_encode($resposta);
+
+	mysqli_close($link);
+
+?>
